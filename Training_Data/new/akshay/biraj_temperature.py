85b0088ff5a4e0bd07ab49453e4cb39c3571f567
Akshay Budhkar <abudhkar@uwaterloo.ca>
2017-03-14 21:55:50 -0400

    Thank you Biraj for adding and leaving the temperature values in the serial data. And thank you for taking full responsibility for this mistake

---
+++ Training_Data/new/akshay/biraj_temperature.py
@@ -0,0 +1,28 @@
+# This is because of Biraj's stupidity
+# His concerns are not valid
+
+import os
+dir = './'
+files = ['hello_akshay.txt', 'your_akshay.txt', 'have_akshay.txt']
+
+new_strings = []
+for name in files:
+    final_string = ""
+    for line in open(dir + name):
+        if line in ["END", "END\n", "END\r\n"]:
+            final_string += "END\n"
+        else:
+            splits = line.split(",")
+            new_splits = []
+            for i, val in enumerate(splits):
+                if i != 6:
+                    new_splits.append(val)
+
+            new_line = ",".join(new_splits)
+            final_string += new_line
+
+    new_strings.append(final_string)
+
+for string in new_strings:
+    print string
+    print "Thank you Biraj"
